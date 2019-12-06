d364570ec9bc0288b4d8511e3ac9b54fcbc3d0ff
Siva Kumar <kavis97@gmail.com>
2019-12-06 11:11:00 +0000

    Initializing spring boot gradle project

---
+++ src/main/java/com/siva/feedmetechtest/feedmetechtest/FeedmetechtestApplication.java
@@ -0,0 +1,13 @@
+package com.siva.feedmetechtest.feedmetechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedmetechtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedmetechtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siva/feedmetechtest/feedmetechtest/FeedmetechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.feedmetechtest.feedmetechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedmetechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
